aecf86255e17b59e31b3dc1d57322927e179af55
Olayinka <adebisiolayinka30@gmail.com>
2023-08-03 14:30:47 +0100

    running pods

--- eks.py
+++ eks.py
@@ -9,20 +9,20 @@ api_client = client.ApiClient()
 
 # Define the deployment
 deployment = client.V1Deployment(
-    metadata=client.V1ObjectMeta(name="flask-app"),
+    metadata=client.V1ObjectMeta(name="flaskapp"),
     spec=client.V1DeploymentSpec(
         replicas=1,
         selector=client.V1LabelSelector(
-            match_labels={"app": "flask-app"}
+            match_labels={"app": "flaskapp2"}
         ),
         template=client.V1PodTemplateSpec(
             metadata=client.V1ObjectMeta(
-                labels={"app": "flask-app"}
+                labels={"app": "flaskapp2"}
             ),
             spec=client.V1PodSpec(
                 containers=[
                     client.V1Container(
-                        name="flask-container",
+                        name="flask-container2",
                         image="775495980804.dkr.ecr.us-east-1.amazonaws.com/cloud-native-monitoring-app",
                         ports=[client.V1ContainerPort(container_port=5000)]
                     )
@@ -42,9 +42,9 @@ api_instance.create_namespaced_deployment(
 
 # Define the service
 service = client.V1Service(
-    metadata=client.V1ObjectMeta(name="flask-service"),
+    metadata=client.V1ObjectMeta(name="flaskservice"),
     spec=client.V1ServiceSpec(
-        selector={"app": "flask-app"},
+        selector={"app": "flaskapp2"},
         ports=[client.V1ServicePort(port=5000)]
     )
 )
